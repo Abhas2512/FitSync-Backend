ab305b57a9ff06f91d61649ca170fd2d0eb49a93
Abhas <abhaskumarbardhan@gmail.com>
2026-07-06 19:16:11 +0530

    Allow Vercel Frontend

--- src/main/java/com/project/fitness/security/SecurityConfig.java
+++ src/main/java/com/project/fitness/security/SecurityConfig.java
@@ -45,7 +45,10 @@ public class SecurityConfig {
         CorsConfiguration configuration = new CorsConfiguration();
 
         configuration.setAllowedOrigins(
-                List.of("http://localhost:5173")
+                List.of(
+                        "http://localhost:5173",
+                        "https://fitsync-frontend-chi.vercel.app"
+                )
         );
 
         configuration.setAllowedMethods(
